87101228d627a4cbc4297dfc22d3d0c5ca55ae07
Víctor Jesús Torres Ferreyra <vjtorresf@outlook.es>
2024-04-03 13:32:21 -0600

    Initial Commit

---
+++ prog.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main (void) {
+	printf("Generacion de codigo de 3 direcciones\n");
+	int x,y,i,j,z;
+	x = 34;
+	y = x * 34 - (x - 12 + 13);
+	
+	i = 0;
+	do {
+		i = i + 1 + 0;
+	} while (i < 10);
+
+	while (i < 15) {
+		i = i + 1 + 0;
+	}
+
+	for (int k = 0; k < i; k++) {
+		x = x + 1;
+	}
+
+	return y;
+}
